cfbcea4f8d508d048c1434fd55c1c700b7fa6a70
mrx7355608 <mrx7355608@gmail.com>
2024-01-12 18:36:10 +0500

    added basic operations

--- main.c
+++ main.c
@@ -3,24 +3,38 @@
 void printLogo();
 void printMainMenu();
 int getUserInput();
+void handleBasicOperations();
+float getNumInput(char[]);
 
 int main() {
     printLogo();
-}
-
-void printLogo() {
-    printf("Calculator");
-    printf("==========");
 
     printMainMenu();
     int choice = getUserInput();
+
+    switch (choice) {
+        case 1:
+            handleBasicOperations();
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+    }
+}
+
+void printLogo() {
+    printf("Calculator\n");
+    printf("==========\n");
 }
 
 void printMainMenu() {
-    printf("1. Basic operations");
-    printf("2. Trignometric operations");
-    printf("3. Algebric operations");
-    printf("4. Logarithmic operations");
+    printf("1. Basic operations\n");
+    printf("2. Trignometric operations\n");
+    printf("3. Algebric operations\n");
+    printf("4. Logarithmic operations\n");
 }
 
 int getUserInput() {
@@ -29,3 +43,38 @@ int getUserInput() {
     scanf("%d", &choice);
     return choice;
 }
+
+float getNumInput(char msg[]) {
+    int num;
+    printf("%s", msg);
+    scanf("%d", &num);
+    return num;
+}
+
+void handleBasicOperations() {
+    printf("1. Add\n");
+    printf("2. Subtract\n");
+    printf("3. Multiply\n");
+    printf("4. Divide\n");
+    int choice = getUserInput();
+
+    float n1 = getNumInput("Enter number 1: ");
+    float n2 = getNumInput("Enter number 2: ");
+
+    switch (choice) {
+        case 1:
+            printf("Answer: %f", n1 + n2);
+            break;
+        case 2:
+            printf("Answer: %f", n1 - n2);
+            break;
+        case 3:
+            printf("Answer: %f", n1 * n2);
+            break;
+        case 4:
+            printf("Answer: %.2f", n1 / n2);
+            break;
+
+    }
+
+};
