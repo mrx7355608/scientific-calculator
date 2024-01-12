d8dd6a97c66a966b82de508e47c2aa3dc88c6a93
mrx7355608 <mrx7355608@gmail.com>
2024-01-12 18:14:06 +0500

    taking user input

--- main.c
+++ main.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 
+void printLogo();
+void printMainMenu();
+int getUserInput();
+
 int main() {
+    printLogo();
+}
+
+void printLogo() {
     printf("Calculator");
+    printf("==========");
+
+    printMainMenu();
+    int choice = getUserInput();
+}
+
+void printMainMenu() {
+    printf("1. Basic operations");
+    printf("2. Trignometric operations");
+    printf("3. Algebric operations");
+    printf("4. Logarithmic operations");
+}
+
+int getUserInput() {
+    int choice;
+    printf("Choose: ");
+    scanf("%d", &choice);
+    return choice;
 }
