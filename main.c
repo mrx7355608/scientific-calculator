ce427fe6a06e61b40bf947b09f2d483d8ead6c3c
mrx7355608 <109037712+mrx7355608@users.noreply.github.com>
2024-01-13 00:28:46 +0500

    added algebric functions

--- main.c
+++ main.c
@@ -10,6 +10,11 @@ float getNumInput(char[]);
 void clearScreen();
 void handleBasicOperations();
 void handleTrignoOperations();
+void handleAlgebricOperations();
+float exponentiation(float);
+float nthRoot(float, float);
+int factorial(int);
+float absoluteValue(float);
 
 // Main function
 int main() {
@@ -26,6 +31,7 @@ int main() {
         	handleTrignoOperations();
             break;
         case 3:
+        	handleAlgebricOperations();
             break;
         case 4:
             break;
@@ -78,7 +84,6 @@ void handleTrignoOperations() {
 	// Print calculator
 	printLogo();
 	
-	// Print basic operations menu
     printf("\n1. Sin\n");
     printf("2. Cos\n");
     printf("3. Tan\n");
@@ -111,6 +116,73 @@ void handleTrignoOperations() {
 
     }
 }
+
+void handleAlgebricOperations() {
+	// Clear screen
+	clearScreen();
+	
+	// Print calculator
+	printLogo();
+	
+    printf("\n1. Exponentiation\n");
+    printf("2. Square root\n");
+    printf("3. Cube root\n");
+    printf("4. Nth root\n");
+    printf("5. Factorial\n");
+    printf("6. Absolute value\n");
+    int choice = getUserInput();
+
+    float n1 = getNumInput("\nEnter number: ");
+
+    switch (choice) {
+        case 1:
+            printf("Answer: %.2f\n", exponentiation(n1));
+            break;
+        case 2:
+            printf("Answer: %.2f\n", sqrt(n1));
+            break;
+        case 3:
+            printf("Answer: %.2f\n", cbrt(n1));
+            break;
+        case 4: {
+        	float nth = getNumInput("\nEnter number (n): ");
+            printf("Answer: %.2f\n", nthRoot(n1, nth));
+            break;
+
+		}
+        case 5:
+            printf("Answer: %.2f\n", factorial(n1));
+            break;
+        case 6:
+            printf("Answer: %.2f\n", absoluteValue(n1));
+            break;
+    }
+
+};
+
+/*
+	@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+		CALCULATOR FUNCTIONS
+	@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+*/
+float exponentiation(float num) {
+	/* 
+		TODO: implement this function
+	*/
+	return 1.0;
+};
+
+float nthRoot(float num, float n) {
+	float answer = pow(num, 1/n);
+	return answer;
+};
+
+int factorial(int num) {
+};
+
+float absoluteValue(float num) {
+};
+
 /*
 	@@@@@@@@@@@@@@@@@@@@@@@@@@
 		DISPLAY FUNCTIONS
